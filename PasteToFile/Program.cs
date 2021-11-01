5006e2e3b9331d0b2e1e2b808dfcb6759d560746
Omar Naman <onaman@uwaterloo.ca>
2021-11-01 17:27:42 -0400

    Initial Commit

---
+++ PasteToFile/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+
+namespace PasteToFile
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length < 1)
+                {
+                    Console.WriteLine("No path passed");
+                    return;
+                }
+                string FolderPath = args[0];
+                if (Clipboard.ContainsImage())
+                {
+                    Console.WriteLine("Image");
+                    SaveImage(FolderPath);
+                }
+                else if (Clipboard.ContainsText())
+                {
+                    Console.WriteLine("Text");
+                    SaveText(FolderPath);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+
+        private static void SaveText(string path)
+        {
+            var now = DateTime.Now;
+            string fileName = $"P2F_{now.Year}{now.Month}{now.Day}-{now.Hour}{now.Minute}{now.Second}.txt";
+            string filePath = Path.Combine(path, fileName);
+            string clipboardText = Clipboard.GetText();
+            File.WriteAllText(filePath, clipboardText);
+        }
+        private static void SaveImage(string path)
+        {
+            var now = DateTime.Now;
+            string fileName = $"P2F_{now.Year}{now.Month}{now.Day}-{now.Hour}{now.Minute}{now.Second}.jpg";
+            string filePath = Path.Combine(path, fileName);
+            var clipboardText = Clipboard.GetImage();
+            clipboardText.Save(filePath, System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+    }
+}
